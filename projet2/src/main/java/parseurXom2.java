60d57344725b2488672305ea124f150a25f4fbd2
ismail <senhaji.ismail@gmail.com>
2013-02-27 17:08:08 +0100

    #1 commit 1 du bug

--- projet2/src/main/java/parseurXom2.java
+++ projet2/src/main/java/parseurXom2.java
@@ -9,7 +9,7 @@ public class parseurXom2 {
 		// TODO Auto-generated method stub
 		   
 		    Element root = new Element("root");    
-		    root.appendChild("Correction bug !");
+		    root.appendChild("Correction bug test !");
 		    Document doc = new Document(root);
 		    String result = doc.toXML();
 		    System.out.println(result);
